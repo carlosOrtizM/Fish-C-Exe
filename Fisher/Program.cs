96a87cac32eab1fac14b1e7d1c5bf3677100ae9e
carlosOrtizM <164468596+carlosOrtizM@users.noreply.github.com>
2024-10-29 17:23:10 -0600

    Add files via upload

---
+++ Fisher/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace Fisher
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            IntroSplashForm splashForm = new IntroSplashForm();
+            IntroSplashForm.ShowSplashScreen(splashForm);
+
+            int milliseconds = 15000;
+            Thread.Sleep(milliseconds);
+
+            Encumbrance login = new Encumbrance();
+            IntroSplashForm.CloseForm(splashForm);
+
+            Application.Run(login);
+        }
+    }
+}
